2d76d5247b79b06c26e1a3a57f31170f05ae9e80
eminokumus1 <eminokumus1@gmail.com>
2025-02-26 16:55:05 +0300

    New properties added to Book model. Book model implements Parcelable interface

--- app/src/main/java/com/eminokumus/bookvox/model/Book.kt
+++ app/src/main/java/com/eminokumus/bookvox/model/Book.kt
@@ -1,8 +1,15 @@
 package com.eminokumus.bookvox.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Book(
-    val id: Int,
-    val name: String,
-    val author: String,
-    val imageUrl: String
-)
+    val id: Int? = null,
+    val name: String? = null,
+    val author: String? = null,
+    val imageUrl: String? = null,
+    val summary: String? = null,
+    val audioUrl: String? = null,
+    val content: String? = null // Kitabın tam içeriği (şimdilik test verisi olacak)
+): Parcelable
