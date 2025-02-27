31a1d8596757f5683ccc3531ad44a66b72a27519
eminokumus1 <eminokumus1@gmail.com>
2025-02-27 11:10:43 +0300

    Book Properties displayed in UI using data binding.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-parcelize")
+    id("kotlin-kapt")
     id("androidx.navigation.safeargs.kotlin")
 }
 
@@ -29,8 +30,9 @@ android {
         }
     }
 
-    buildFeatures{
+    buildFeatures {
         viewBinding = true
+        dataBinding = true
     }
 
     compileOptions {
@@ -52,10 +54,10 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.8.7")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.8.7")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.7")
 
-    implementation ("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
--- app/src/main/java/com/eminokumus/bookvox/bookdetails/BookDetailsFragment.kt
+++ app/src/main/java/com/eminokumus/bookvox/bookdetails/BookDetailsFragment.kt
@@ -5,18 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.eminokumus.bookvox.R
+import com.eminokumus.bookvox.databinding.FragmentBookDetailsBinding
 
 
 class BookDetailsFragment : Fragment() {
+    private lateinit var binding: FragmentBookDetailsBinding
+    private lateinit var viewModel: BookDetailsViewModel
 
+    val args: BookDetailsFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_details, container, false)
+    ): View {
+        binding = FragmentBookDetailsBinding.inflate(layoutInflater, container, false)
+
+        viewModel = BookDetailsViewModel()
+        viewModel.setBook(args.book)
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        return binding.root
     }
 
 
