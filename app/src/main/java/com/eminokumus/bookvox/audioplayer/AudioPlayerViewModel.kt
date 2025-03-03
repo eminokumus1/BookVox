0683fbb2563fd181ab5a4d511ea96282b2fccd0b
eminokumus1 <eminokumus1@gmail.com>
2025-03-03 14:40:02 +0300

    CurrentTime and TotalTime displaying format changed.

--- app/src/main/java/com/eminokumus/bookvox/audioplayer/AudioPlayerViewModel.kt
+++ app/src/main/java/com/eminokumus/bookvox/audioplayer/AudioPlayerViewModel.kt
@@ -6,7 +6,7 @@ import androidx.lifecycle.ViewModel
 import com.eminokumus.bookvox.model.Book
 import kotlin.text.StringBuilder
 
-class AudioPlayerViewModel: ViewModel() {
+class AudioPlayerViewModel : ViewModel() {
     private val _book = MutableLiveData<Book>()
     val book: LiveData<Book> get() = _book
 
@@ -19,31 +19,36 @@ class AudioPlayerViewModel: ViewModel() {
     private val _currentTimeInMillis = MutableLiveData<Long>()
     val currentTimeInMillis: LiveData<Long> get() = _currentTimeInMillis
 
-    fun setBook(book: Book){
+    fun setBook(book: Book) {
         _book.value = book
     }
 
-    fun setAudioDuration(durationInMillis: Long){
+    fun setAudioDuration(durationInMillis: Long) {
         _audioDurationInMinutes.value = millisToMinutesString(durationInMillis)
     }
 
-    private fun millisToMinutesString(millis: Long): String{
+    private fun millisToMinutesString(millis: Long): String {
         val seconds = millisToSeconds(millis)
         val minutes = seconds.div(60).toInt()
-        val remainSeconds = seconds%60
-        val minutesInString = StringBuilder().append(minutes).append(":").append(remainSeconds).toString()
-        return minutesInString
+        val remainSeconds = seconds % 60
+        val hours = seconds.div(3600) % 24
+        if (hours > 0) {
+            return String.format("%02d:%02d:%02d", hours, minutes, remainSeconds)
+        } else {
+            return String.format("%02d:%02d", minutes, remainSeconds)
+        }
+
     }
 
-    fun millisToSeconds(num: Long): Long{
-        return num/1000
+    fun millisToSeconds(num: Long): Long {
+        return num / 1000
     }
 
-    fun secondsToMillis(num: Int): Long{
+    fun secondsToMillis(num: Int): Long {
         return (num * 1000).toLong()
     }
 
-    fun updateCurrentTime(millis: Long){
+    fun updateCurrentTime(millis: Long) {
         _currentTimeInMinutes.value = millisToMinutesString(millis)
         _currentTimeInMillis.value = millis
     }
